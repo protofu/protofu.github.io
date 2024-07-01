aa616cb6a752cce667cbf8cdd4eaa4f407d5e9b1
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-01 18:26:23 +0900

    feat : pages

---
+++ my_page/src/pages/RendingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RendingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Rending Page</h1>
+      </div>
+    );
+  }
+}
+
+export default RendingPage;
